eb6b53cc091cfa9d8d63ccfa0d70a08fb6835795
karianyu <mibangoe041@gmail.com>
2026-09-04 10:23:06 +0300

    custom css errors

--- erpnext/startup/boot.py
+++ erpnext/startup/boot.py
@@ -12,7 +12,11 @@ from erpnext.accounts.utils import get_fiscal_years
 def boot_session(bootinfo):
 	"""boot session - send website info if guest"""
 
-	bootinfo.custom_css = frappe.db.get_value("Style Settings", None, "custom_css") or ""
+	bootinfo.custom_css = ""
+	try:
+		bootinfo.custom_css = frappe.db.get_value("Style Settings", None, "custom_css")
+	except Exception as e:
+		pass
 
 	if frappe.session["user"] != "Guest":
 		update_page_info(bootinfo)
